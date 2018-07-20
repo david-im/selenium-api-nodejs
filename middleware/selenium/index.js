34e4353a03e4efa58d43fe7d8136805d3774f76f
David Im <david@ntcon.co>
2018-07-20 14:43:02 +0900

    각 드라이버의 status가 관리되도록 설정 및 ready instance 로직 적용

--- middleware/selenium/index.js
+++ middleware/selenium/index.js
@@ -1,8 +1,26 @@
 'use strict';
+const driverCount = process.env.DRIVER_COUNT || 5; // 이 서비스가 가질 driver개수를 환경변수에서 가져옴(default : 5)
+const isHeadless = (process.env.HEADLESS === 'true') || true; // 이 서비스에서 사용되는 driver가 headless로 동작할지의 여부를 환경변수에서 가져옴(default : true)
 
 const webdriver = require('selenium-webdriver');
 const chrome = require('selenium-webdriver/chrome');
-const driver = new webdriver.Builder().forBrowser('chrome').build();
-// const driver = new webdriver.Builder().forBrowser('chrome').setChromeOptions(new chrome.chrome.Options().addArguments('--headless')).build();
+const drivers = [];
 
-module.exports = driver;
+// driverCount만큼 driver생성
+for (let i = 0; i < driverCount; i++) {
+    const driver = new webdriver.Builder().forBrowser('chrome');
+
+    if(isHeadless) {
+        driver.setChromeOptions(new chrome.Options().addArguments('--headless')) //실제 GUI 브라우저를 안쓰고 처리
+    }
+    const driverInstance = driver.build();
+
+    driverInstance.id = i + 1;
+    driverInstance.isReady = true; // 이 드라이버가 쉬고있는지 isReady라는 변수 설정
+    drivers.push(driverInstance);
+}
+
+module.exports = {
+    readyDrivers: () => drivers.filter(driver => driver.isReady),
+    findReadyDriver: () => drivers.find(driver => driver.isReady)
+};
